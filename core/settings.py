c464d6e4744b17f10222dce9d54b8a84a4fd222b
l1k4skr <82717429+l1k4skr@users.noreply.github.com>
2023-11-05 03:01:40 -0300

    Sesion 7.1

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from os import path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    path.join(BASE_DIR, 'myapp/static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- myapp/views.py
+++ myapp/views.py
@@ -21,52 +21,52 @@ VIEWS
 # Create your views here.
 # Index view
 def index(request):
-    return render(request ,"index.html")
+    return render(request ,"html/index.html")
 
 # Home view
 def home(request):
-    return render(request ,"home.html")
+    return render(request ,"html/home.html")
 
 # Login view
 def login(request):
-    return render(request ,"login.html")
+    return render(request ,"html/login.html")
 
 # Reset_password view
 def reset_password(request):
-    return render(request ,"reset_password.html")
+    return render(request ,"html/reset_password.html")
 
 class Client_view:
 
     # Client view
     def client(self, request):
-        return render(request ,"client.html")
+        return render(request ,"html/client.html")
 
     # New client view
     def new_client(self, request):
-        return render(request ,"new_client.html")
+        return render(request ,"html/new_client.html")
 
 # Trazability view
 def trazability(request):
-    return render(request ,"trazability.html")
+    return render(request ,"html/trazability.html")
 
 class Machine_view:
     # Machine view
     def machine(request):
-        return render(request ,"machine.html")
+        return render(request ,"html/machine.html")
     # New machine view
     def new_machine(request):
-        return render(request ,"new_machine.html")
+        return render(request ,"html/new_machine.html")
 
 class Manual_view:
     # Manual view
     def manual(request):
-        return render(request ,"manual.html")
+        return render(request ,"html/manual.html")
     def new_manual(request):
-        return render(request ,"new_manual.html")
+        return render(request ,"html/new_manual.html")
 
 
 
 # Create an about view
 def about(request):
-    return render(request ,"about.html")
+    return render(request ,"html/about.html")
 
